df7d56c445ad7f99cebe56169efd7ce9cbf88902
Joan Miquel Torres <joanmiquel@mallorcaweb.net>
2014-11-08 16:09:27 +0100

    Personalyzed icon.

---
+++ client/apk/bin/updateIcon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+srcFile=res/icon.png
+
+for targetFile in $(find platforms/ -iname icon.png); do
+
+	echo "Replacing ${targetFile}..."
+	geom=$(identify $targetFile | awk '{print $3}')
+
+	convert $srcFile -resize $geom $targetFile
+
+done;
+
